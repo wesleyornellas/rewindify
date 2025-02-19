f73e3f28610458cfd7f97bf7d320ddce586b0352
wesleyornellas <wesleyornellas@outlook.com>
2025-02-19 10:30:59 -0300

    add project

---
+++ src/main/java/com/wesley/rewindify/RewindifyApplication.java
@@ -0,0 +1,13 @@
+package com.wesley.rewindify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewindifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewindifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesley/rewindify/RewindifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.rewindify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewindifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
